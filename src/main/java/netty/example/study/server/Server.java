b4bb19a8de36b89dc0a46d03d19ba0914d29e183
ALBERT <huiweb01@163.com>
2021-05-10 14:44:48 +0800

    group、handler添加名称(调试时增加可读性)

--- src/main/java/netty/example/study/server/Server.java
+++ src/main/java/netty/example/study/server/Server.java
@@ -9,6 +9,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
 import lombok.extern.slf4j.Slf4j;
 import netty.example.study.server.codec.OrderFrameDecode;
 import netty.example.study.server.codec.OrderFrameEncode;
@@ -28,25 +29,31 @@ public class Server {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ServerBootstrap b = new ServerBootstrap();
         b.channel(NioServerSocketChannel.class)
-//                .option(NioChannelOption.SO_BACKLOG, 1024)
+//                .option(NioChannelOption.SO_BACKLOG, 1024) // 最大连接等待数量
 //                .childOption(NioChannelOption.TCP_NODELAY, true)
-                .handler(new LoggingHandler(LogLevel.INFO)) // 为NioServerSocketChannel添加 ChannelHandler
-                .group(new NioEventLoopGroup());
+                .handler(new LoggingHandler(LogLevel.INFO)); // 为NioServerSocketChannel添加 ChannelHandler
+
+        // 线程
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(0, new DefaultThreadFactory("boss"));
+        NioEventLoopGroup workGroup = new NioEventLoopGroup(0, new DefaultThreadFactory("worker"));
+
+        b.group(bossGroup, workGroup);
+
 
         // channel顺序，入站：自上而下；出站：自下而上，对于服务端而言，先执行入站操作，而对于客户端而言，先执行出站操作。
         b.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) throws Exception {
                 ChannelPipeline pipeline = ch.pipeline();
-                pipeline.addLast(new OrderFrameDecode()); // 入站
-                pipeline.addLast(new OrderFrameEncode()); // 出站
-                pipeline.addLast(new OrderProtocolEncode()); // 出站
-                pipeline.addLast(new OrderProtocolDecode()); // 入站
 
-                pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+                pipeline.addLast("frameDecoder", new OrderFrameDecode()); // 入站
+                pipeline.addLast("frameEncode", new OrderFrameEncode()); // 出站
+                pipeline.addLast("protocolEncode", new OrderProtocolEncode()); // 出站
+                pipeline.addLast("protocolDecode", new OrderProtocolDecode()); // 入站
 
-                pipeline.addLast(new OrderServerProcessHandler()); // 入站
+                pipeline.addLast("loggingHandler", new LoggingHandler(LogLevel.INFO));
 
+                pipeline.addLast("orderServerProcessHandler", new OrderServerProcessHandler()); // 入站
             }
         });
 
